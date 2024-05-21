a004027c4e7898d05ea56c0c7d6d97e609a1632f
avd <aravind19112003@gmail.com>
2024-05-21 22:52:01 +0530

    PUT method with RequestParser

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument(
+    "name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument(
+    "views", type=int, help="Number of views of the video", required=True)
+video_put_args.add_argument(
+    "likes", type=int, help="Number of likes of the video", required=True)
 
-class Greet(Resource):
-
-    def get(self, name):
-
-        return {"data": name}
-
-    def post(self):
-
-        return {"data": "Posted!"}
+videos = {}
 
 
-api.add_resource(Greet, "/greet/<string:name>")
+class Video(Resource):
+    def get(self, video_id):
+        if video_id not in videos:
+            return {"message": "Video not found"}, 404
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,8 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "greet/aravind")
+# Put request to add a video
+response = requests.put(
+    BASE + "video/1", json={"name": "xyz", "views": 990, "likes": 99})
 print(response.json())
 
-# response = requests.post(BASE + "greet")
-# print(response.json())
+# Get request to retrieve the video data
+response = requests.get(BASE + "video/1")
+print(response.json())
