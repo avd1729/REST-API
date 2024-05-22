19740aedc9b7b5a4fc5300576b80fab7e5c57dba
avd <aravind19112003@gmail.com>
2024-05-23 01:07:04 +0530

    Add UPDATE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -31,35 +31,64 @@ video_put_args.add_argument(
 video_put_args.add_argument(
     "likes", type=int, help="Number of likes of the video", required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument(
+    "name", type=str, help="Name of the video", required=True)
+video_update_args.add_argument(
+    "views", type=int, help="Number of views of the video")
+video_update_args.add_argument(
+    "likes", type=int, help="Number of likes of the video")
 
-
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, "Video already exists with this ID...")
-
-
-def abort_if_video_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, "Could not find video...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 
 class Video(Resource):
+
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        if video_id not in videos:
-            return {"message": "Video not found"}, 404
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_exists(video_id=video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
 
-    def delete(self, video_id):
-        abort_if_video_doesnt_exist(video_id=video_id)
-        del videos[video_id]
-        return '', 204
+        if result:
+            abort(409, message="Video ID taken...")
+        video = VideoModel(
+            id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
+
+        '''
+        This code block can be optimized.
+        '''
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.name = args['views']
+        if args['likes']:
+            result.name = args['likes']
+
+        db.session.commit()
+        return result
 
 
 api.add_resource(Video, "/video/<int:video_id>")
