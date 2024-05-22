b75eccdd385357f9888edc43a780ea8f5779053b
avd <aravind19112003@gmail.com>
2024-05-23 00:15:50 +0530

    Add aborts & DELETE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,6 +15,16 @@ video_put_args.add_argument(
 videos = {}
 
 
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, "Video already exists with this ID...")
+
+
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, "Could not find video...")
+
+
 class Video(Resource):
     def get(self, video_id):
         if video_id not in videos:
@@ -22,9 +32,15 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id=video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id=video_id)
+        del videos[video_id]
+        return '', 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
